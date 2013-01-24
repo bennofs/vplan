8f4bd88ec364035e338cc4e9200d39cb8aa089a2
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2013-01-24 20:42:39 +0100

    Basic schedule datastructures
    Implement Schedule.hs

--- src/Core/Schedule.hs
+++ src/Core/Schedule.hs
@@ -1,12 +1,45 @@
-module Core.Schedule2 where
+{-# LANGUAGE GeneralizedNewtypeDeriving, TemplateHaskell #-}
+module Core.Schedule where
 
-import Core.Timespan
 import Core.Course
+import Control.Monad
+import Data.Monoid
+import Data.List
+import Control.Lens
 
-newtype LessonNr = LessonNr Int
-data Date d = Date d | After (Timespan d)
+type Range d = (d,d)
+data Item d a = Item { _skipRange :: [Range d], _repeatEvery :: d, _validRange :: (Range d), _val :: a}
+makeLenses ''Item
+type Schedule = [Item Int [Course]]
 
-data Modifier d  = RepeatEvery (Timespan d) | Start (Date d) | End (Date d) | Except (Date d)
-data Action d a = Action [Modifier d] a
-type Schedule = [Action (Date Int) [Course]]
+inRange :: (Ord d) => d -> Range d -> Bool
+inRange x (s,e) = x >= s && x < e
 
+rangeLength :: (Num d) => Range d -> d
+rangeLength (x,y) = y - x
+
+isInnerRangeOf :: (Ord d) => Range d -> Range d -> Bool
+isInnerRangeOf (s1, e1) r = s1 `inRange` r || e1 `inRange` r
+
+skip :: (Num d, Ord d) => d -> [Range d] -> d -> d
+skip x es d
+  | null es' = d + x
+  | otherwise = skip (x + skipExtra es) (es \\ es') d
+  where es' = filter (`isInnerRangeOf` (d, d + x)) es
+        skipExtra = sum . map rangeLength
+
+iAt :: (Num d, Ord d, MonadPlus m) => Item d a -> d -> m a
+iAt i d
+  | not $ d `inRange` (i^.validRange) = mzero
+  | d == i^.validRange._1 = return $ i^.val
+  | otherwise = flip iAt d $ i & validRange._1 .~ next
+  where next = skip (i^.repeatEvery) (i^.skipRange) (i^.validRange._1)
+
+iShiftStart :: (Num d) => d -> Item d a -> Item d a
+iShiftStart = over (validRange._1) . (+)
+
+sAt :: Schedule -> Int -> [Course]
+sAt = flip $ \d -> maybe [] id . getFirst . mconcat . map (First . (`iAt` d))
+
+makeWeekSchedule :: Range Int -> [[Course]] -> Schedule
+makeWeekSchedule v = zipWith iShiftStart [0..] .  map (Item [] 7 v)
