f519f932cefd306ea0d8d8f1e4400ed80078846e
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2013-03-20 19:57:03 +0100

    Remove unnecessary functions from Constant.hs

--- src/Core/Modifier/Constant.hs
+++ src/Core/Modifier/Constant.hs
@@ -14,7 +14,6 @@
 
 module Core.Modifier.Constant (
     Constant(..)
-  , value
   , constant
   ) where
 
@@ -26,15 +25,10 @@ import Control.Applicative
 -- | A modifier that always returns the same value, no matter to what it is applied.
 newtype Constant a b = Constant a deriving (Functor, Show)
 makeIso ''Constant
-instance Wrapped a b (Constant a c) (Constant b d) where wrapped = constant
 
 instance (Monoid m) => Applicative (Constant m) where
   pure _ = Constant mempty
   (Constant x) <*> (Constant y) = Constant $ x <> y
-
--- | Make a const modifier that returns the given value
-value :: a -> Constant a b
-value = Constant
 
 instance (Monoid a) => Modifier (Constant a b) i a where
   modifierApply (Constant a) _ = const a
